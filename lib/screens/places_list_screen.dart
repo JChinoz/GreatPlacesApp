fac74a094914688d411160f41612f248e430ccb5
Jitki Chin <jitkichin@era.com.sg>
2021-01-28 15:22:35 +0800

    Added Native Device Camera functionality and storage

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:GreatPlaces/providers/great_places.dart';
 import 'package:GreatPlaces/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -17,7 +19,23 @@ class PlacesListScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Consumer<GreatPlaces>(
+          child: Center(
+            child: Text('Got no places yet, start adding some!'),
+          ),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : ListView.builder(
+                  itemCount: greatPlaces.items.length,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(greatPlaces.items[i].image),
+                    ),
+                    title: Text(greatPlaces.items[i].title),
+                    onTap: () {},
+                  ),
+                ),
+        ),
       ),
     );
   }
